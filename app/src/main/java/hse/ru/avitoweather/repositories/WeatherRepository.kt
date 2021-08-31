bc18936df2b4682fe63707582c2bf0c44ac77970
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2021-08-31 21:21:34 +0300

    second commit

--- app/src/main/java/hse/ru/avitoweather/repositories/WeatherRepository.kt
+++ app/src/main/java/hse/ru/avitoweather/repositories/WeatherRepository.kt
@@ -1,4 +1,28 @@
 package hse.ru.avitoweather.repositories
 
+import androidx.annotation.NonNull
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class WeatherRepository {
+    private var apiService: ApiService = ApiClient.getRetrofit().create(ApiService::class.java)
+
+    fun registerAccount(login: String): LiveData<LoginResponse> {
+        val data: MutableLiveData<LoginResponse> = MutableLiveData()
+        apiService.getRegistrationCode(login).enqueue(object : Callback<LoginResponse> {
+            override fun onFailure(@NonNull call: Call<LoginResponse>, t: Throwable) {
+                data.value = null
+            }
+            override fun onResponse(
+                @NonNull call: Call<LoginResponse>,
+                @NonNull response: Response<LoginResponse>
+            ) {
+                data.value = response.body()
+            }
+        })
+        return data
+    }
 }
--- app/src/main/java/hse/ru/avitoweather/viewmodels/WeatherViewModel.kt
+++ app/src/main/java/hse/ru/avitoweather/viewmodels/WeatherViewModel.kt
@@ -4,12 +4,14 @@ import android.app.Application
 import androidx.annotation.NonNull
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import hse.ru.avitoweather.repositories.WeatherRepository
+import hse.ru.avitoweather.responses.WeatherResponse
 
 class WeatherViewModel(@NonNull application: Application) : AndroidViewModel(application) {
     private var loginRepository: WeatherRepository =
         WeatherRepository()
 
-    fun registerAccount(login:String): LiveData<LoginResponse> {
+    fun registerAccount(login:String): LiveData<WeatherResponse> {
         return loginRepository.registerAccount(login)
     }
 
