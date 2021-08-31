57ccbfdb9f3ed79c1541a4882f5e1008deb41a0a
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2021-08-31 21:03:27 +0300

    Initial commit

---
+++ app/src/main/java/hse/ru/avitoweather/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package hse.ru.avitoweather.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hse.ru.avitoweather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
